f39169663b895d0272c460c26a22edf0f375c62c
Matheus <wyvern800@hotmail.com>
2019-01-08 17:45:54 -0200

    #Update 12

--- index.php
+++ index.php
@@ -9,34 +9,67 @@
     <hr />
 
 <?php if ($db) : ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">Olá, <strong><?php echo isset($_SESSION) ? $_SESSION['UserName'] : 'Null'?></strong>. Seja muito bem vindo ao Dashboard. - Escolha o que quer deseja fazer agora!</div>
+        <div class="panel-body">
+            <div class="col-xs-6 col-sm-3 col-md-2">
+                <a href="users" class="btn btn-default">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-11 text-center">
+                            <p>Usuários</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
 
-    <div class="row">
-        <div class="col-xs-6 col-sm-3 col-md-2">
-            <a href="users/add.php" class="btn btn-primary">
-                <div class="row">
-                    <div class="col-xs-12 text-center">
-                        <i class="fa fa-plus fa-5x"></i>
+            <div class="col-xs-6 col-sm-3 col-md-2">
+                <a href="category" class="btn btn-default">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-12 text-center">
+                            <p>Categorias</br>de Comandos</p>
+                        </div>
                     </div>
-                    <div class="col-xs-12 text-center">
-                        <p>Novo Usuário</p>
-                    </div>
-                </div>
-            </a>
-        </div>
+                </a>
+            </div>
 
-        <div class="col-xs-6 col-sm-3 col-md-2">
-            <a href="users" class="btn btn-default">
-                <div class="row">
-                    <div class="col-xs-12 text-center">
-                        <i class="fa fa-user fa-5x"></i>
+                <div class="col-xs-6 col-sm-3 col-md-2">
+                <a href="commands" class="btn btn-default">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-12 text-center">
+                            <p>Comandos</p>
+                        </div>
                     </div>
-                    <div class="col-xs-12 text-center">
-                        <p>Usuários</p>
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-sm-3 col-md-2">
+                <a href="news" class="btn btn-default">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-12 text-center">
+                            <p>Notícias</p>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
+
+            </div>
+
         </div>
     </div>
+
+
 
 
 
